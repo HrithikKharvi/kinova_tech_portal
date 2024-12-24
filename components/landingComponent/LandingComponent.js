2bccab59cf19dec8b45404d07d87b535f7f8c51b
Hrithik R <hrithikkharvi9@gmail.com>
2024-12-25 01:08:18 +0530

    font-size fix

--- components/landingComponent/LandingComponent.js
+++ components/landingComponent/LandingComponent.js
@@ -1,7 +1,7 @@
-import { loadHTML, loadCss} from '../commonFunctions.js';
+import { loadHTML, loadCss, loadJs} from '../commonFunctions.js';
 
 async function loadJsCss() {
-    await loadHTML("components/navbar/navbar.html", "page-navbar");
+    await loadHTML("components/navbar/navbar.html", "page-navbar", () => loadJs("components/navbar/navbar.js"));
     await loadCss("components/landingComponent/LandingComponent.css");
 }
 
