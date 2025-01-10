f3d608c6f252212d60010349fc17ce6467cd567d
Hrithik R <hrithikkharvi9@gmail.com>
2025-01-11 02:08:32 +0530

    Modified how we work and added contact us

---
+++ components/contactUs/contactUs.js
@@ -0,0 +1,7 @@
+import {loadCss} from '../commonFunctions.js';
+
+async function loadJsCss() {
+    await loadCss("components/contactUs/contactUs.css");
+}
+
+loadJsCss();
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     loadHTML("components/aboutUs/aboutUs.html", "about-us", () => loadJs("components/aboutUs/aboutUs.js"));
     loadHTML("components/footer/Footer.html", "footer", () => loadJs("components/footer/Footer.js"));
     // loadHTML("components/blogs/blogs.html", "blogs", () => loadJs("components/blogs/blogs.js"));
-    loadHTML("components/howwework/howwework.html", "howwework", () => loadJs("components/howwework/howwework.js"));
+    loadHTML("components/howwework2/howwework.html", "howwework", () => loadJs("components/howwework2/howwework.js"));
     loadHTML("components/whatwedo/whatwedo.html", "whatwedoAsCompany", () => loadJs("components/whatwedo/whatwedo.js"));
     loadHTML("components/blogsSlider/blogsSlider.html", "blogsSlider", () => loadJs("components/blogsSlider/blogsSlider.js"));
+    loadHTML("components/contactUs/contactUs.html", "contactUs", () => loadJs("components/contactUs/contactUs.js"));
+
 });
